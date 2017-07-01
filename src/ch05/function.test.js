cd05feb5932b1fef0db88b333ff7d2c839104079
Larry Jones <larryj@blinds.com>
2017-06-30 22:11:56 -0500

    Additional `function` tests:
    * Has `length` property
    * Has a prototype
    * The prototype of it's prototype is `Object.prototype`
    * Function.prototype has `apply` method
    * Function.prototype has `call` method

--- src/ch05/function.test.js
+++ src/ch05/function.test.js
@@ -3,23 +3,43 @@
  */
 
 describe('function', () => {
-  it('is an object to which one can add properties', () => {
-    const assert = (() => {
-      const result = (expr, message) => {
-        if (!expr) {
-          throw new Error(message);
-        }
-        
-        result.count++;
-        
-        return true;
-      };
-      result.count = 0;
+  const assert = (() => {
+    const result = (expr, message) => {
+      if (!expr) {
+        throw new Error(message);
+      }
       
-      return result;
-    })();
+      result.count++;
+      
+      return true;
+    };
+    result.count = 0;
     
+    return result;
+  })();
+  
+  it('is an object to which one can add properties', () => {
     assert(1);
     expect(assert.count).toBe(1);
+  });
+  
+  it('has a length property whose value is the arity of the function', () => {
+    expect(assert.length).toBe(2);
+  });
+  
+  it('has a prototype', () => {
+    expect(Object.getPrototypeOf(assert)).toBe(Function.prototype);
+  });
+  
+  it('has a prototype whose prototype is Object.prototype', () => {
+    expect(Object.getPrototypeOf(Object.getPrototypeOf(assert))).toBe(Object.prototype);
+  });
+  
+  it('has a prototype with `apply`', () => {
+    expect(Object.getOwnPropertyNames(Function.prototype)).toContain('apply');
+  });
+  
+  it('has a prototype with `call`', () => {
+    expect(Object.getOwnPropertyNames(Function.prototype)).toContain('call');
   });
 });
